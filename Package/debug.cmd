35edf05034a2acf08cfce19db4a3d1503badaf4c
sbwalker <shaun.walker@siliqon.com>
2024-01-29 07:41:59 -0500

    upgrade to .NET 8

--- Package/debug.cmd
+++ Package/debug.cmd
@@ -1,8 +1,8 @@
-XCOPY "..\Client\bin\Debug\net7.0\DNF.Projects.Client.Oqtane.dll" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net7.0\" /Y
-XCOPY "..\Client\bin\Debug\net7.0\DNF.Projects.Client.Oqtane.pdb" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net7.0\" /Y
-XCOPY "..\Server\bin\Debug\net7.0\DNF.Projects.Server.Oqtane.dll" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net7.0\" /Y
-XCOPY "..\Server\bin\Debug\net7.0\DNF.Projects.Server.Oqtane.pdb" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net7.0\" /Y
-XCOPY "..\Server\bin\Debug\net7.0\RestSharp.dll" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net7.0\" /Y
-XCOPY "..\Shared\bin\Debug\net7.0\DNF.Projects.Shared.Oqtane.dll" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net7.0\" /Y
-XCOPY "..\Shared\bin\Debug\net7.0\DNF.Projects.Shared.Oqtane.pdb" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net7.0\" /Y
+XCOPY "..\Client\bin\Debug\net8.0\DNF.Projects.Client.Oqtane.dll" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net8.0\" /Y
+XCOPY "..\Client\bin\Debug\net8.0\DNF.Projects.Client.Oqtane.pdb" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net8.0\" /Y
+XCOPY "..\Server\bin\Debug\net8.0\DNF.Projects.Server.Oqtane.dll" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net8.0\" /Y
+XCOPY "..\Server\bin\Debug\net8.0\DNF.Projects.Server.Oqtane.pdb" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net8.0\" /Y
+XCOPY "..\Server\bin\Debug\net8.0\RestSharp.dll" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net8.0\" /Y
+XCOPY "..\Shared\bin\Debug\net8.0\DNF.Projects.Shared.Oqtane.dll" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net8.0\" /Y
+XCOPY "..\Shared\bin\Debug\net8.0\DNF.Projects.Shared.Oqtane.pdb" "..\..\oqtane.framework\Oqtane.Server\bin\Debug\net8.0\" /Y
 XCOPY "..\Server\wwwroot\Modules\DNF.Projects\*" "..\..\oqtane.framework\Oqtane.Server\wwwroot\Modules\DNF.Projects\" /Y /S /I
